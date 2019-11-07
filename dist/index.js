3c6ff63245291071d20c7d5f61ae2435e71e855b
Brett Mandler <brettmandler@gmail.com>
2019-11-07 08:37:56 -0500

    Cleanup

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var rambda_1 = require("rambda");
-exports.makeConfig = function (config) { return function (a) {
-    return rambda_1.merge(a.config, config);
-}; };
